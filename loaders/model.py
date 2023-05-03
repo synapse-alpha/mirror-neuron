8d384ec19d79914b291d2bb4eb2fe3f082f9ebdb
Steffen Cruz <steffenjcruz@gmail.com>
2023-05-03 11:25:42 -0600

    Add simple example model training to illustrate how gradients and parameters are uploaded to wandb

--- loaders/model.py
+++ loaders/model.py
@@ -40,6 +40,7 @@ def load_model(bt_config=None, **kwargs):
     template = ModelConfigTemplate(**wandb.config.model)
     print(f'Template: {template}')
 
+    run_watch_experiment()
     dendrite_pool = _load_model_from_module(base.dendrite_pool, 'dendrite_pool', **kwargs)
     wandb.watch(dendrite_pool, log='all')
     gating_model = _load_model_from_module(base.gating, 'gating_model', **kwargs)
@@ -57,3 +58,51 @@ def load_model(bt_config=None, **kwargs):
     print(f'Made model:\n{model}')
 
     return model
+
+def run_watch_experiment():
+        
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    import numpy as np
+
+    # Define a simple sequential model
+    model = nn.Sequential(
+        nn.Linear(2, 16),
+        nn.ReLU(),
+        nn.Linear(16, 1)
+    )
+    wandb.watch(model, log='all', log_freq=10, log_graph=True)
+    
+
+    # Define a loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+    # Generate a synthetic dataset
+    np.random.seed(1234)
+    x = np.random.rand(1000, 2)
+    y = np.sin(x[:, 0] + x[:, 1])
+
+    # Convert numpy arrays to PyTorch tensors
+    x = torch.from_numpy(x).float()
+    y = torch.from_numpy(y).float()
+
+    example_ct = 0  # number of examples seen
+    # Train the model
+    for epoch in range(100):
+        # Forward pass
+        y_pred = model(x)
+        loss = criterion(y_pred.squeeze(), y)
+        
+        # Backward pass and optimization step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        example_ct +=  len(x)
+        # Print the loss every 10 epochs
+        if epoch % 10 == 0:
+            wandb.log({"epoch": epoch, "loss": loss}, step=example_ct)
+            print(f'Epoch {epoch}, Loss: {loss.item():.4f}')
+
+    
